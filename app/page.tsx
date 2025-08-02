44b5eda8008cf93e7b6936d74d3ca69965a43680
bedjoi <obedindevu@gmail.com>
2025-08-02 22:01:30 +0200

    feat:Cleaning the start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 
 export default function Home() {
+  const x = 2;
+  const y = 3;
+  const sum = x + y;
+  console.log(`The sum of ${x} and ${y} is ${sum}`);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900 ">
       <h1>Welcome to Glomed Academia</h1>
       <Image
-        src="/images/hero.jpg"
+        src={"/hero.png"}
         alt="Hero Image"
-        width={500}
-        height={300}
+        width={200}
+        height={200}
+        className="rounded-lg shadow-lg bg-amber-400 bg-cover bg-center"
       />
     </div>
   );
