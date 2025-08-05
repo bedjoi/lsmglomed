68f98756a3088002821a12d9b1156e4629914428
bedjoi <obedindevu@gmail.com>
2025-08-06 01:25:52 +0200

    feat: adding the teacher mode button and the exit button

--- app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
@@ -1,10 +1,19 @@
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Menu } from 'lucide-react'
 import React from 'react'
+import Sidebar from './sidebar'
 
 const MobileSidebar = () => {
   return (
       <div>
-          <Menu />
+          <Sheet>
+              <SheetTrigger className="md:hidden fixed pr-4 hover:opacity-75 transition-opacity z-50">
+                  <Menu />
+              </SheetTrigger>
+              <SheetContent side="left" className="bg-white">
+                  <Sidebar />
+              </SheetContent>
+          </Sheet>
     </div>
   )
 }
