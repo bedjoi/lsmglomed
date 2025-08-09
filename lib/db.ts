82dce3c51c2b25e188b2e69ff849ea22d07abead
bedjoi <obedindevu@gmail.com>
2025-08-09 12:50:16 +0200

    feat: using of the msqldb

--- lib/db.ts
+++ lib/db.ts
@@ -5,6 +5,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma 
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
