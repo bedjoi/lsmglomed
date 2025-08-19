70a79153f686c59e79e6de0d99b4cab7d7ee410d
bedjoi <obedindevu@gmail.com>
2025-08-19 12:01:05 +0200

    fix:fix dependances

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
 // prisma db
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+    prisma: PrismaClient | undefined;
+};
 
 export const db = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    allowedDevOrigins: ["local-origin.dev", "26c1c7eece35.ngrok-free.app"],
+
+    /* config options here */
 };
 
 export default nextConfig;
