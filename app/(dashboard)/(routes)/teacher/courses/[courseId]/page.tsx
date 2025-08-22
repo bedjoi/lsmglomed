a57134537904f5289dc9a9d9b3b4e905ad76fe69
bedjoi <obedindevu@gmail.com>
2025-08-22 23:35:50 +0200

    feat:remove unused import

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,7 +5,6 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
-import ImageEdgeForm from "./_components/image-edge-form";
 
 interface PageProps {
     params: Promise<{ courseId: string }>;
