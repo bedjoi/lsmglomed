0a8b4a0e2612072c22d24c7994d2f7d151f80a9e
bedjoi <obedindevu@gmail.com>
2025-09-17 00:39:06 +0200

    feat:creating the categorymap courses

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+import SearchPage from "./search/page";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900 ">
-      <UserButton   />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900 ">
+            <SearchPage />
+        </div>
+    );
 }
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import React from "react";
+import { Categories } from "./_components/categories";
 
-const SearchPage = () => {
-  return (
-    <div>this is the SearchPage</div>
-  )
-}
+const SearchPage = async () => {
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
+    return (
+        <div className="p-6 space-y-4">
+            <Categories items={categories} />
+        </div>
+    );
+};
 
-export default SearchPage
+export default SearchPage;
