94b744dbb89472656544bc9665a43e36258ea574
bedjoi <obedindevu@gmail.com>
2025-09-17 02:11:47 +0200

    fix:fixing a menu opening

--- app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
@@ -1,21 +1,30 @@
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import { Menu } from 'lucide-react'
-import React from 'react'
-import Sidebar from './sidebar'
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+    SheetTitle,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import React from "react";
+import Sidebar from "./sidebar";
+import { VisuallyHidden } from "@/components/ui/visually-hidden";
 
 const MobileSidebar = () => {
-  return (
-      <div>
-          <Sheet>
-              <SheetTrigger className="md:hidden fixed pr-4 hover:opacity-75 transition-opacity z-50">
-                  <Menu />
-              </SheetTrigger>
-              <SheetContent side="left" className="bg-white">
-                  <Sidebar />
-              </SheetContent>
-          </Sheet>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Sheet>
+                <SheetTrigger className="md:hidden fixed pr-4 hover:opacity-75 transition-opacity z-50">
+                    <Menu />
+                </SheetTrigger>
+                <SheetContent side="left" className="bg-white">
+                    <VisuallyHidden>
+                        <SheetTitle>Navigation Menu</SheetTitle>
+                    </VisuallyHidden>
+                    <Sidebar />
+                </SheetContent>
+            </Sheet>
+        </div>
+    );
+};
 
-export default MobileSidebar
+export default MobileSidebar;
