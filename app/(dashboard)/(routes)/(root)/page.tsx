eb2c660febc935252ed38df8c97266635727e55d
bedjoi <obedindevu@gmail.com>
2025-09-24 16:25:29 +0200

    feat:finishing the dashboard of user

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import { getDashboardCourses } from "@/actions/get-dashboard-courses";
 import { CoursesList } from "@/components/corses-list";
 import { redirect } from "next/navigation";
+import { CheckCircle, Clock } from "lucide-react";
+import { InfoCard } from "./_components/info_card";
 
 export default async function Dashboard() {
     const { userId } = await auth();
@@ -14,16 +16,17 @@ export default async function Dashboard() {
     return (
         <div className="p-6 space-y-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div>
-                    {
-                        // TODO: info Card
-                    }{" "}
-                </div>
-                <div>
-                    {
-                        // TODO: info Card
-                    }{" "}
-                </div>
+                <InfoCard
+                    icon={Clock}
+                    label="In Progress"
+                    numberOfItems={coursesInProgress.length}
+                />
+                <InfoCard
+                    icon={CheckCircle}
+                    label="Completed"
+                    numberOfItems={completedCourses.length}
+                    variant="success"
+                />
             </div>
             <CoursesList items={[...coursesInProgress, ...completedCourses]} />
         </div>
